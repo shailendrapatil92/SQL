73aeaf190c76118e947b3f23d89b0fb7c2ad7356
Shailendra Patil <shailendrapatil@shailendras-mbp.attlocal.net>
2018-04-01 23:59:24 -0400

    Removed extra query

--- Stanford SQL/social_network.sql
+++ Stanford SQL/social_network.sql
@@ -188,16 +188,6 @@ where
 	h3.id = f2.id1 and
 	f2.id2 =l1.id2
 
-	
-select distinct H1.name, H1.grade, H2.name, H2.grade, H3.name, H3.grade
-from Highschooler H1, Likes, Highschooler H2, Highschooler H3, Friend F1, Friend F2
-where H1.ID = Likes.ID1 and Likes.ID2 = H2.ID and
-  H2.ID not in (select ID2 from Friend where ID1 = H1.ID) and
-  H1.ID = F1.ID1 and F1.ID2 = H3.ID and
-  H3.ID = F2.ID1 and F2.ID2 = H2.ID;
-
-
-
 
 
 
